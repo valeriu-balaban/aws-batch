d85a252afa9177a5fb0001c0263e824c299d6d58
Valeriu Balaban <vbalaban@usc.edu>
2017-03-18 21:43:27 -0700

    Separates actions

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Switch user
-su -l ec2-user
-
-EC2_INSTANCE_ID="`curl http://169.254.169.254/latest/meta-data/instance-id`"
-touch $EC2_INSTANCE_ID
-aws s3 cp $EC2_INSTANCE_ID s3://batch-instances/
-aws ec2 terminate-instances --instance-id $EC2_INSTANCE_ID
